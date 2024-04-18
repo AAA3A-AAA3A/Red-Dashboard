205f2bbf90b71d221def14728ebae7cd76492ed3
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-04-18 13:50:25 +0200

    Bump version.

--- reddash/__init__.py
+++ reddash/__init__.py
@@ -4,7 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 Copyright (c) 2020 - present Neuro Assassin (https://github.com/Cog-Creators/Red-Dashboard)
 """
 
-__version__ = "1.3.1"
+__version__ = "1.4.0"
 __authos__ = ["Neuro Assassin", "AAA3A"]
 
 from .app import FlaskApp
