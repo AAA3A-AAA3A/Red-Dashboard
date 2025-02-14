abdf3ccbb1eb8c9f7ad5d6cb7e8131bed3098988
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2025-02-14 21:17:05 +0100

    Bump version.

--- reddash/__init__.py
+++ reddash/__init__.py
@@ -6,7 +6,7 @@ Copyright (c) 2020 - present Neuro Assassin (https://github.com/Cog-Creators/Red
 
 import typing
 
-__version__: str = "1.7.1"
+__version__: str = "1.7.2"
 __authors__: typing.List[str] = ["AAA3A", "Neuro Assassin"]
 
 from .app import FlaskApp
